59f6f866be6924d032edb765d8c886c3b9a808d9
kuba342 <j.derda010@gmail.com>
2021-11-28 22:40:24 +0100

    Repositories

--- src/main/java/com/przychodniamk2/database/repositories/ElementsOfTreatmentRepository.java
+++ src/main/java/com/przychodniamk2/database/repositories/ElementsOfTreatmentRepository.java
@@ -1,10 +1,10 @@
 package com.przychodniamk2.database.repositories;
 
-import com.przychodniamk2.database.orm.tables.Personals;
+import com.przychodniamk2.database.orm.tables.ElementsOfTreatment;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PersonalsRepository extends CrudRepository<Personals, Long> {
+public interface ElementsOfTreatmentRepository extends CrudRepository<ElementsOfTreatment, Long> {
 
 }
--- src/main/java/com/przychodniamk2/database/repositories/PositionsRepository.java
+++ src/main/java/com/przychodniamk2/database/repositories/PositionsRepository.java
@@ -1,10 +1,10 @@
 package com.przychodniamk2.database.repositories;
 
-import com.przychodniamk2.database.orm.tables.Personals;
+import com.przychodniamk2.database.orm.tables.Positions;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PersonalsRepository extends CrudRepository<Personals, Long> {
+public interface PositionsRepository extends CrudRepository<Positions, Long> {
 
 }
