f9cf729da4f80b235dc8b531710510f451032dad
kuba342 <j.derda010@gmail.com>
2021-11-28 16:36:45 +0100

    Generated classes for SQL views

--- src/main/java/com/przychodniamk2/database/orm/views/ScheduledVisitsView.java
+++ src/main/java/com/przychodniamk2/database/orm/views/ScheduledVisitsView.java
@@ -1,18 +1,51 @@
 package com.przychodniamk2.database.orm.views;
 
+import org.hibernate.annotations.Immutable;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.sql.Date;
 
 @Entity
+@Immutable
 public class ScheduledVisitsView {
-    private Long id;
+    //Tutaj jedynie gettery porobione
+    //https://thorben-janssen.com/spring-data-jpa-views/
+    @Id
+    private Long ID;
 
-    public void setId(Long id) {
-        this.id = id;
+    private java.sql.Date date;
+    private String doctorsFirstName;
+    private String doctorsLastName;
+    private String patientsPESEL;
+    private String patientsFirstName;
+    private String patientsLastName;
+
+    public Long getID() {
+        return ID;
     }
 
-    @Id
-    public Long getId() {
-        return id;
+    public Date getDate() {
+        return date;
+    }
+
+    public String getDoctorsFirstName() {
+        return doctorsFirstName;
+    }
+
+    public String getDoctorsLastName() {
+        return doctorsLastName;
+    }
+
+    public String getPatientsPESEL() {
+        return patientsPESEL;
+    }
+
+    public String getPatientsFirstName() {
+        return patientsFirstName;
+    }
+
+    public String getPatientsLastName() {
+        return patientsLastName;
     }
 }
