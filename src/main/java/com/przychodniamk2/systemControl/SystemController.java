af37b130aa4636b76a28aa41b8e5fef0baf82dba
karolWazny <63009342+karolWazny@users.noreply.github.com>
2021-11-29 19:50:15 +0100

    added SystemController

--- src/main/java/com/przychodniamk2/systemControl/SystemController.java
+++ src/main/java/com/przychodniamk2/systemControl/SystemController.java
@@ -1,4 +1,48 @@
 package com.przychodniamk2.systemControl;
 
+import com.przychodniamk2.business.Date;
+import com.przychodniamk2.business.Doctor;
+import com.przychodniamk2.business.Person;
+import com.przychodniamk2.business.ScheduledVisit;
+
 public class SystemController implements UserInteractionController {
+    @Override
+    public Person choosePatient() {
+        return null;
+    }
+
+    @Override
+    public Date chooseDate() {
+        return null;
+    }
+
+    @Override
+    public Doctor chooseDoctor() {
+        return null;
+    }
+
+    @Override
+    public void addDoctor(Doctor doctor) {
+
+    }
+
+    @Override
+    public void addVisit(ScheduledVisit visit) {
+
+    }
+
+    @Override
+    public ScheduledVisit chooseVisit() {
+        return null;
+    }
+
+    @Override
+    public void performVisit(ScheduledVisit visit) {
+
+    }
+
+    @Override
+    public void addPatient(Person patient) {
+
+    }
 }
--- src/main/java/com/przychodniamk2/systemControl/UserInteractionController.java
+++ src/main/java/com/przychodniamk2/systemControl/UserInteractionController.java
@@ -1,5 +1,17 @@
 package com.przychodniamk2.systemControl;
 
-public interface UserInteractionController {
+import com.przychodniamk2.business.Date;
+import com.przychodniamk2.business.Doctor;
+import com.przychodniamk2.business.Person;
+import com.przychodniamk2.business.ScheduledVisit;
 
+public interface UserInteractionController {
+    Person choosePatient();
+    Date chooseDate();
+    Doctor chooseDoctor();
+    void addDoctor(Doctor doctor);
+    void addVisit(ScheduledVisit visit);
+    ScheduledVisit chooseVisit();
+    void performVisit(ScheduledVisit visit);
+    void addPatient(Person patient);
 }
