bc5ce49cb7523ef52c8313b17147f658e3c7f293
karolWazny <63009342+karolWazny@users.noreply.github.com>
2021-12-16 20:35:05 +0100

    added fxml elements

--- src/main/java/com/przychodniamk2/gui/browseplannedvisits/BrowsePlannedVisitsController.java
+++ src/main/java/com/przychodniamk2/gui/browseplannedvisits/BrowsePlannedVisitsController.java
@@ -3,6 +3,8 @@ package com.przychodniamk2.gui.browseplannedvisits;
 import com.przychodniamk2.business.Doctor;
 import com.przychodniamk2.business.ScheduledVisit;
 import com.przychodniamk2.gui.FXMLController;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
 import org.springframework.context.ApplicationContext;
 
 public class BrowsePlannedVisitsController extends FXMLController<ScheduledVisit> {
@@ -20,5 +22,16 @@ public class BrowsePlannedVisitsController extends FXMLController<ScheduledVisit
     @Override
     public void setData(ScheduledVisit scheduledVisit){
         doctor = scheduledVisit.getDoctor();
+    }
+
+    @FXML
+    private void cancelClick(MouseEvent event){
+        close();
+    }
+
+    @FXML
+    private void confirmClick(MouseEvent event){
+
+        close();
     }
 }
