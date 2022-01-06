8aa9ee61520376da1c6bc44d5e7d5e43b3c8f54b
karolWazny <63009342+karolWazny@users.noreply.github.com>
2022-01-06 01:17:01 +0100

    gui side without logic under it

--- src/main/java/com/przychodniamk2/gui/changepassword/ChangePasswordController.java
+++ src/main/java/com/przychodniamk2/gui/changepassword/ChangePasswordController.java
@@ -2,10 +2,21 @@ package com.przychodniamk2.gui.changepassword;
 
 import com.przychodniamk2.business.User;
 import com.przychodniamk2.gui.FXMLController;
+import com.przychodniamk2.systemControl.usecase.LogInService;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import org.springframework.context.ApplicationContext;
 
 public class ChangePasswordController extends FXMLController<User> {
     private final static String fxml = "changePassword.fxml";
+    private User user;
+    private ApplicationContext context;
+
+    @FXML
+    private Label loginLabel;
+
+    @FXML
+    private Label userLabel;
 
     public ChangePasswordController() {
         super(fxml);
@@ -13,6 +24,24 @@ public class ChangePasswordController extends FXMLController<User> {
 
     @Override
     public void setContext(ApplicationContext context) {
+        this.context = context;
+        this.user = context.getBean("logInService", LogInService.class).getLoggedUser();
 
+        initializeLabels();
+    }
+
+    private void initializeLabels(){
+        loginLabel.textProperty().set(user.getUsername());
+        userLabel.textProperty().set("" + user.getEmployee());
+    }
+
+    @FXML
+    private void confirmClick(){
+        close();
+    }
+
+    @FXML
+    private void cancelClick(){
+        close();
     }
 }
