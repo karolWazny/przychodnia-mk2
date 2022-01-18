81f25771b44a69029981b132f9e440a5ca31a2ed
karolWazny <63009342+karolWazny@users.noreply.github.com>
2022-01-18 18:06:16 +0100

    first test fixture

---
+++ src/test/java/fitnesse/FirstFixture.java
@@ -0,0 +1,26 @@
+package fitnesse;
+
+public class FirstFixture {
+    private int firstNumber;
+    private int secondNumber;
+
+    public int getFirstNumber() {
+        return firstNumber;
+    }
+
+    public void setFirstNumber(int firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public int getSecondNumber() {
+        return secondNumber;
+    }
+
+    public void setSecondNumber(int secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    public int sum(){
+        return firstNumber + secondNumber;
+    }
+}
