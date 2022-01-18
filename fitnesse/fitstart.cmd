198d0ae41098c9fb7e22aac385b53a584ef53d09
karolWazny <63009342+karolWazny@users.noreply.github.com>
2022-01-18 18:32:43 +0100

    configurable port in startup file

--- fitnesse/fitstart.cmd
+++ fitnesse/fitstart.cmd
@@ -1,2 +1,5 @@
-start cmd /k java -jar fitnesse-standalone.jar -p 8080
-explorer "http://localhost:8080"
+set "port=8081"
+REM start new fitnesse server on specified port - in new cmd window, so we can still use this one
+start cmd /k java -jar fitnesse-standalone.jar -p %port%
+REM open browser and connect with just started FitNesse server on the same port
+explorer "http://localhost:%port%"
