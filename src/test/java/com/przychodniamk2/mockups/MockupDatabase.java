998862fc6a8129aea295e140c80fc5fc187d0476
karolWazny <63009342+karolWazny@users.noreply.github.com>
2022-01-19 13:29:36 +0100

    deleted obsolete imports

--- src/test/java/com/przychodniamk2/mockups/MockupDatabase.java
+++ src/test/java/com/przychodniamk2/mockups/MockupDatabase.java
@@ -1,10 +1,5 @@
 package com.przychodniamk2.mockups;
 
-import com.przychodniamk2.business.*;
-import com.przychodniamk2.systemControl.database.Database;
-import org.springframework.context.ApplicationContext;
-
-import java.util.List;
 
 public class MockupDatabase extends MockupDatabaseBase {
 
