e5efe8f2b30cd77caa9f7af7e9bc520e05ed3993
karolWazny <63009342+karolWazny@users.noreply.github.com>
2022-01-21 14:11:45 +0100

    created setUp class

---
+++ src/test/java/com/przychodniamk2/acceptance/SetUp.java
@@ -0,0 +1,19 @@
+package com.przychodniamk2.acceptance;
+
+import com.przychodniamk2.mockups.MockupContextImpl;
+import fit.Fixture;
+import org.springframework.context.ApplicationContext;
+
+public class SetUp extends Fixture {
+    static ApplicationContext context;
+
+    public SetUp(){
+        MockupContextImpl contextImpl = new MockupContextImpl();
+
+        context = contextImpl;
+    }
+
+    public ApplicationContext getContext(){
+        return null;
+    }
+}
